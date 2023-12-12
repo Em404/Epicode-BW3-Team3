b4d173af5856270664780e6c4d4da17ea83421ff
AlessandroForestiero <forestieroalessandro94@gmail.com>
2023-12-12 11:24:28 +0100

    home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -10,13 +10,25 @@ import { IProducts } from './models/i-products';
 export class HomeComponent {
 
   product!: IProducts[];
-
+  styleProduct!:any[]
+  isButtonHovered:boolean=false;
   constructor(private homeService: HomeService) {}
 
   ngOnInit() {
     this.homeService.getAllProducts().subscribe(products => {
       this.product = products;
+     this.styleProduct = [...this.product];
+     for(let p of this.styleProduct) p.hover=false
     });
+  }
+
+  onButtonMouseOver(i:number):void{
+    this.styleProduct[i].hover=true;
+    console.log(this.styleProduct);
+
+  }
+  onButtonMouseOut(i:number):void{
+    this.styleProduct[i].hover=false;
   }
 
 }
